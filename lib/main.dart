c9e3d06197034d21194c6ea94245bb49157e0037
thurakhant <thurakhantthein4425@gmail.com>
2022-04-23 02:37:28 +0630

    adding chat screen and function

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ void main() {
   runApp(const FriendlyChatApp());
 }
 
+String _name = 'Your Name';
+
 class FriendlyChatApp extends StatelessWidget {
   const FriendlyChatApp({
     Key? key,
@@ -18,16 +20,123 @@ class FriendlyChatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatMessage extends StatelessWidget {
+  final String text;
+  const ChatMessage({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16),
+            child: CircleAvatar(
+              child: Text(_name[0]),
+            ),
+          ),
+          Column(
+            children: [
+              Text(
+                _name,
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 5),
+                child: Text(text),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final List<ChatMessage> _message = [];
+  final _textController = TextEditingController();
+  final FocusNode _focusNode = FocusNode();
+
+  void _handleSubmmited(String text) {
+    _textController.clear();
+    var message = ChatMessage(text: text);
+    setState(() {
+      _message.insert(0, message);
+      _focusNode.requestFocus();
+    });
+  }
+
+  Widget _buildTextComposer() {
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).colorScheme.secondary),
+      child: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          children: [
+            Flexible(
+              child: TextField(
+                focusNode: _focusNode,
+                controller: _textController,
+                onSubmitted: _handleSubmmited,
+                decoration:
+                    const InputDecoration.collapsed(hintText: 'Send Message'),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              child: IconButton(
+                  onPressed: () => _handleSubmmited(_textController.text),
+                  icon: const Icon(Icons.send)),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('FriendlyChat'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('FriendlyChat'),
+        ),
+        body: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(
+                itemBuilder: (i, index) => _message[index],
+                itemCount: _message.length,
+                padding: const EdgeInsets.all(8),
+                reverse: true,
+              ),
+            ),
+            const Divider(
+              height: 1.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+              ),
+              child: _buildTextComposer(),
+            ),
+          ],
+        ),
       ),
     );
   }
